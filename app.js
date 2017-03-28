a7156dcbc1cf6636c59c06fbd45f992ecb64f1bd
Collin Vossman <cvossman@ksu.edu>
2017-03-28 12:31:35 -0500

    forgotten commit from class

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
 "use strict";
 
-var PORT = 3000;
 
-var fs = require('fs');
-var http = require('http');
 var sqlite3 = require('sqlite3').verbose();
+var require('http');
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
 
 var router = new (require('./lib/route').Router);
 
-router.get('/', function(req, res) {
-  fs.readFile('public/index.html', function(err, body){
+router.add('/app.js', function(req, res) {
+  fs.readFile('public/app.js', function(err, body) {
     res.end(body);
-  });
-});
-
-router.get('/app.js', function(req, res) {
-  fs.readFile('public/app.js', function(err, body){
-    res.end(body);
-  });
-});
-
-router.get('/projects', function(req, res) {
-  db.all('SELECT * FROM projects', [], function(err, projects){
-    res.setHeader('Content-Type', 'text/json');
-    res.end(JSON.stringify(projects));
   });
 });
 
 var migrate = require('./lib/migrate');
-migrate(db, 'migrations', function(err){
+migrate(db, 'migrations', function(err) {
 
   var server = new http.Server(function(req, res) {
-    router.route(req, res);
+    router.route(req, res)
   });
-  server.listen(PORT, function(){
-    console.log("listening on port " + PORT);
+  server.listen(3000, function() {
+    console.log("Listening on port " + 3000);
   });
-
-
 });
